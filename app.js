a9e01ac7e5093f338dfae4034bc506bad67b37a4
PabloCamejo <111792708+PabloCamejo@users.noreply.github.com>
2022-09-12 13:08:19 -0300

    Agregando enrutado

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3030;
+const app = express();
+app.use(express.static('public'))
+
+
+app.set('views', './views/pages')
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register')
+});
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.get('/cart', (req, res) => {
+    res.render('cart')
+});
+
+app.get('/products', (req, res) => {
+    res.render('product')
+})
+
+
+app.listen(PORT, () => {
+    console.log('http://localhost:3030');
+})
